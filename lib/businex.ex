d65c696e7074477f4548ec0f27fa03d6830af5d6
Phil McClure <phil.mcclure@gmail.com>
2018-02-20 14:34:30 +0000

    Initial commit

---
+++ lib/businex.ex
@@ -0,0 +1,18 @@
+defmodule Businex do
+  @moduledoc """
+  Documentation for Businex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Businex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/businex_test.exs
@@ -0,0 +1,8 @@
+defmodule BusinexTest do
+  use ExUnit.Case
+  doctest Businex
+
+  test "greets the world" do
+    assert Businex.hello() == :world
+  end
+end
